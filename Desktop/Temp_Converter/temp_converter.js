fe18f23e8bca679972080369ceb5d4962e985b6f
Srishtisinha630 <srishtisinha630@gmail.com>
2023-06-14 22:28:15 +0530

    commit third

--- Desktop/Temp_Converter/temp_converter.js
+++ Desktop/Temp_Converter/temp_converter.js
@@ -1,35 +1,30 @@
-const calculateTemp = () => {
-
-    const numberTemp = document.getElementById('temp').value;
-    
-    const tempSelected = document.getElementById('temp_diff');
-    console.log(JSON.stringify(tempSelected));
-    alert(JSON.stringify(tempSelected))
+const calculateTemp = (e) => {
+    e.preventDefault();
+    const numberTemp = document.getElementById("temp").value;
+  
+    const tempSelected = document.getElementById("temp_diff");
     const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
-    
+  
     const celToFah = (cel) => {
-        let fahrenheit = Math.round((cel * 9/5) + 32);
-        return fahrenheit;
-    }
-     
+      let fahrenheit = Math.round((cel * 9) / 5 + 32);
+      return fahrenheit;
+    };
+  
     const fehToCel = (fehr) => {
-        let celsius  = Math.round((fehr - 32)*5/9);
-        return celsius;
-    }
+      let celsius = Math.round(((fehr - 32) * 5) / 9);
+      return celsius;
+    };
     let result;
-
-    if(valueTemp='cel'){
-        result=celToFah(numberTemp);
-
-        document.getElementById('resultContainer').innerHTML=` ${result}`;
-
-    }else{
-        result=fehToCel(numberTemp);
-        document.getElementById('resultContainer').innerHTML= `${result}`;
-
+  
+    if (valueTemp === "cel") {
+      result = celToFah(numberTemp);
+  
+      document.getElementById("resultContainer").innerHTML = ` ${result} °F`;
+    } else {
+      result = fehToCel(numberTemp);
+      document.getElementById("resultContainer").innerHTML = `${result} °C`;
     }
-
-
-
-
-}
+    return false;
+  };
+  
+  document.querySelector("#tempCalc").addEventListener("submit", calculateTemp);
